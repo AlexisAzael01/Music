5ed5670ed26b2c90ee186655b5d8103c0616c9d5
AlexisAzael01 <alexisazaelmartinez@gmail.com>
2024-09-26 23:06:38 -0700

    music

---
+++ static/js/loading.js
@@ -0,0 +1,32 @@
+songListened = [];
+/*
+function LoadSongs() {
+    const div = document.createElement("div");
+    div.classList.add("songs-container");
+    div.innerHTML = `
+        <table class="music-list border-solid">
+            <thead>
+                <tr>
+                    <td>id</td>
+                    <td>Imagen</td>
+                    <td>Canción</td>
+                    <td>Autor</td>
+                    <td>Descargar</td>
+                </tr>
+            </thead>
+            <tbody id="song-table-body">
+                <tr>
+                </tr>
+            </tbody>
+        </table>
+    `;
+    
+    // Selecciona el elemento donde quieres agregar el div
+    document.body.appendChild(div); // O reemplaza "document.body" por otro contenedor si es necesario
+    console.log("hola");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    LoadSongs();
+});
+*/
